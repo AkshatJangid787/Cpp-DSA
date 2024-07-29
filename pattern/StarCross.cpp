b63c79195a49a61c2960257b00c582cefb5b21fb
Akshat <akshatjangid14@gmail.com>
2024-07-29 22:49:38 +0530

    added

--- pattern/StarCross.cpp
+++ pattern/StarCross.cpp
@@ -5,15 +5,12 @@ using namespace std;
 
 void starPlus(int n)
 {
-    int mid = n / 2 + 1;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (mid == i || mid == j)
-                cout << "*" << " ";
-            else
-                cout << " " << " ";
+            if(i==j || i+j==n+1) cout<<"*";
+            else cout<<" ";
         }
         cout << endl;
     }
