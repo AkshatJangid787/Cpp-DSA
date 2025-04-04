ab676d5d9459a058124426e22e3ea6b6012440a4
Akshat <akshatjangid14@gmail.com>
2025-04-04 11:39:29 +0530

    one pass 1832

--- Strings/SentenceIsPanagram1832.cpp
+++ Strings/SentenceIsPanagram1832.cpp
@@ -16,3 +16,22 @@ class Solution {
            return true;
         }
     };
+
+// 2nd method in one pass
+
+class Solution {
+    public:
+        bool checkIfPangram(string sentence) {
+           vector<int> arr(26, 0);
+           int count=0;
+    
+           for(char &ch: sentence){
+            int index=ch - 'a';
+            if(arr[index]==0){
+                arr[index]++;
+                count++;
+            }
+           } 
+           return (count==26) ? true : false;
+        }
+    };
