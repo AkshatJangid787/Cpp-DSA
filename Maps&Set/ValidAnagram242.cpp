9f8c489bcee015ef02963dd6d68150173e2d7df0
akshatjangid787 <akshatjangid14@gmail.com>
2025-06-02 12:01:38 +0530

    242

---
+++ Maps&Set/ValidAnagram242.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> map;
+        for(int i=0; i<s.length(); i++){
+            map[s[i]]++;
+        }
+        for(int i=0; i<t.length(); i++){
+            char ch = t[i];
+            if(map.find(ch)!=map.end()){
+              map[ch]--;
+              if(map[ch]==0) map.erase(ch);
+            }
+            else return false;
+        }
+        if(map.size()>0) return false;
+
+        return true;
+    }
+};
